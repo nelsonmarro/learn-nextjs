5b99f610349db16e4f8390fe531477ee3d69317d
Nelson Marro <nelsonmarro99@gmail.com>
2025-06-02 18:50:14 -0500

    /workspace/github/nelsonmarro/learn-nextjs

--- 02-my-dashboard/src/app/dashboard/main/page.tsx
+++ 02-my-dashboard/src/app/dashboard/main/page.tsx
@@ -1,6 +1,6 @@
 export default function MainPage() {
   return (
-    <div>
+    <div className="">
       <h1>Hello Page Main</h1>
     </div>
   );
--- teslo-shop/src/app/page.tsx
+++ teslo-shop/src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <main className="bg-blue-500"></main>;
+  return <main className=""></main>;
 }
